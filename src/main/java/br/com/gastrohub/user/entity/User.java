bb3867e8c14c8152a2f8834c9967f23cc640424b
Gabriel Ribeiro <gribeirodelimas2@outlook.com>
2026-04-26 13:32:12 -0300

    Removi a entidade Address duplicada, alinhei os UUIDs para VARCHAR(36) conforme as migrations e configurei MAIL_USER/MAIL_PASSWORD para serem passadas pelo docker-compose.

--- src/main/java/br/com/gastrohub/user/entity/User.java
+++ src/main/java/br/com/gastrohub/user/entity/User.java
@@ -7,6 +7,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -27,6 +29,8 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(length = 36)
     private UUID id;
 
     @Column(name = "nome", length = 150, nullable = false)
